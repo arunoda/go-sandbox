b9a7271f203543a90693f8e6dce1e4b5945b9f0f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-05-28 11:59:03 +0530

    Generates files

--- append/append_test.go
+++ append/append_test.go
@@ -6,6 +6,25 @@ import (
 	"testing"
 )
 
+func TestGenFile(t *testing.T) {
+	f, err := os.OpenFile("data_mmap.txt", os.O_RDWR, 0644)
+	// has no file
+	if err != nil {
+		f, err = os.OpenFile("data_mmap.txt", os.O_RDWR|os.O_CREATE, 0644)
+		check(err)
+		var size int = 1024 * 1024
+
+		for i := 0; i < size; i++ {
+			b := make([]byte, 1024)
+			offset := i * 1024
+			f.WriteAt(b, int64(offset))
+		}
+	}
+
+	f, err = os.OpenFile("data.txt", os.O_RDWR|os.O_CREATE, 0644)
+	check(err)
+}
+
 func BenchmarkAppendFile(b *testing.B) {
 	f, err := os.OpenFile("data.txt", os.O_RDWR, 0644)
 	check(err)
